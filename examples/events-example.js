0ff7f6058cfeb35df34d4c0db98e2aa89f6e8666
Ahmad Khasawneh <ahmad.khasawneh@hotmail.com>
2020-10-24 01:49:04 +0300

    Implement Event emitter + Builder pattern (#7)
    * Implement builder pattern
    
    * Arachnid events MVP
    
    * Implements EventsEmitter + Builder pattern
    
    * normalizd link using toString
    
    * minor fix
    
    * Address comments
    
    * Fixup
    
    used response.statusCode() instead of .status() :facepalm:
    
    * Set values as object instance instead method variable
    
    * Fixup
    
    * Fixup
    
    In pageCrawlingFinished emitting event
    
    * Minor fixups

---
+++ examples/events-example.js
@@ -0,0 +1,26 @@
+const Arachnid = require('../lib/Arachnid');
+
+const domain = 'https://www.example.com/';
+const arachnid = new Arachnid().setDomain(domain).setConcurrency(5).setCrawlDepth(2);
+
+arachnid.on('info', response => console.log('info events', response));
+arachnid.on('results', response => { // returns Map object url: {statusCode, metadata, h1, h2 etc...}
+    console.log('results event'); 
+    for (let [key, value] of response) {
+        console.log(key + ' : ' + value.valueOf());
+    }
+});
+arachnid.on('pageCrawlingStarted', response => {
+    console.log('pageCrawlingStarted events');
+    console.log(response.url);
+});
+arachnid.on('pageCrawlingFinished', response => {
+    console.log('pageCrawlingFinished events');
+    console.log(response);
+});
+arachnid.on('pageCrawlingFailed', response => {
+    console.log('pageCrawlingFailed events');
+    console.log(response.url, response.statusCode);
+});
+
+arachnid.traverse();
---
+++ examples/simple-example.js
@@ -0,0 +1,26 @@
+const Arachnid = require('../lib/Arachnid');
+
+async function crawl(domain, depth, concurrency) {
+    const startTime = Date.now();
+    const arachnidObj = new Arachnid()
+        .setDomain(domain)
+        .setConcurrency(concurrency)
+        .setCrawlDepth(depth)
+        .setPuppeteerParameters([
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--no-sandbox',
+            '--no-zygote',
+            '--single-process',
+        ])
+    const results = await arachnidObj.traverse();
+
+    const endTime = Date.now();
+    console.log(JSON.stringify([...results], null, 4)); 
+    console.log("It took: " + (endTime - startTime) + " milliseconds");
+    console.log(`Found ${results.size} results with depth=${depth} concurrency=${concurrency}`);
+}
+
+crawl("https://laravel-news.com/", 2, 5);
