cac0f643ac85da66c2d5c8ce67dfcd3cbe1f4f2b
Zeid Rashwani <zrashwani@gmail.com>
2020-10-31 20:40:02 +0200

    Performance fixes (#9)
    * merging to master
    
    * removing example.js
    
    * amend spacing
    
    * amend new lines
    
    * amend lines
    
    * fixing logic and modifying example
    
    * newline at end of file
    
    * refactoring variable name and fixing hash issue
    
    * using new browser for each batch instead of new page/tab
    
    * remove event todo

--- examples/events-example.js
+++ examples/events-example.js
@@ -3,24 +3,15 @@ const Arachnid = require('../lib/Arachnid');
 const domain = 'https://www.example.com/';
 const arachnid = new Arachnid().setDomain(domain).setConcurrency(5).setCrawlDepth(2);
 
-arachnid.on('info', response => console.log('info events', response));
 arachnid.on('results', response => { // returns Map object url: {statusCode, metadata, h1, h2 etc...}
-    console.log('results event'); 
-    for (let [key, value] of response) {
-        console.log(key + ' : ' + value.valueOf());
-    }
-});
-arachnid.on('pageCrawlingStarted', response => {
-    console.log('pageCrawlingStarted events');
-    console.log(response.url);
-});
-arachnid.on('pageCrawlingFinished', response => {
-    console.log('pageCrawlingFinished events');
-    console.log(response);
-});
-arachnid.on('pageCrawlingFailed', response => {
-    console.log('pageCrawlingFailed events');
-    console.log(response.url, response.statusCode);
-});
+            console.log('Results received: ', response);
+        })
+        .on('info', response => console.log('INFO:', response))
+        .on('error', err => console.log('ERROR:', err))
+        .on('pageCrawlingStarted', response => console.log('page crawling started: ', response.url))
+        .on('pageCrawlingFinished', response => console.log('page crawling finished: ', response))
+        .on('pageCrawlingFailed', response => console.log('page crawling failed:', response))
+        .on('pageCrawlingSuccessed', response => console.log('page crawling success:', response))
+        .on('pageCrawlingSkipped', response => console.log('page crawling skipped:', response));
 
 arachnid.traverse();
