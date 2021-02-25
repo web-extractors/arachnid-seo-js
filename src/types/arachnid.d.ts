8c33a6e5c51a81f011b77aa88a946fd4846f5035
Zeid Rashwani <zrashwani@gmail.com>
2021-02-25 13:43:21 +0200

    separating logic of BFS crawling from logic of processing single page

--- src/types/arachnid.d.ts
+++ src/types/arachnid.d.ts
@@ -1,88 +1,90 @@
 import { Headers, Response } from 'puppeteer';
 import { URL } from 'url';
+import Link from '../Link';
 import { ExtractedInfo as ExtractedDomInfo } from './mainExtractor';
 
 export interface UrlWithDepth {
-  url: URL;
-  depth: number;
+    url: URL;
+    depth: number;
 }
 
 export interface IndexabilityInfo {
-  isIndexable: boolean;
-  indexabilityStatus: string;
+    isIndexable: boolean;
+    indexabilityStatus: string;
 }
 
 export interface ResultInfo {
-  url: string;
-  urlEncoded: string;
-  statusCode: number;
-  statusText: string;
-  isInternal: boolean;
-  depth: number;
-  isIndexable?: boolean;
-  indexabilityStatus?: string;
-  contentType?: string | null;
-  robotsHeader?: string | null;
-  redirectUrl?: string;
-  DOMInfo?: ExtractedDomInfo;
-  resourceInfo?: PageResourceType[];
+    url: string;
+    urlEncoded: string;
+    statusCode: number;
+    statusText: string;
+    isInternal: boolean;
+    depth: number;
+    isIndexable?: boolean;
+    indexabilityStatus?: string;
+    contentType?: string | null;
+    robotsHeader?: string | null;
+    redirectUrl?: string;
+    DOMInfo?: ExtractedDomInfo;
+    resourceInfo?: PageResourceType[];
+    parentLink?: Link;
 }
 
 export interface CrawlPageResult {
-  url: string;
-  response: Response | ErrorResponse;
-  extractedInfo?: ExtractedDomInfo;
-  resourceInfo?: PageResourceType[];
-  depth: number;
+    link: Link;
+    response: Response | ErrorResponse;
+    extractedInfo?: ExtractedDomInfo;
+    resourceInfo?: PageResourceType[];
+    depth: number;
 }
 
 export interface MetaInfo {
-  title?: string;
-  keywords?: string;
-  author?: string;
-  robots?: string;
-  description?: string;
+    title?: string;
+    keywords?: string;
+    author?: string;
+    robots?: string;
+    description?: string;
 }
 
 export interface ErrorResponse {
-  url(): string;
-  status(): number;
-  statusText(): string;
-  headers(): Headers;
+    url(): string;
+    status(): number;
+    statusText(): string;
+    headers(): Headers;
 }
 
 export interface UrlsToVisitQ {
-  url: URL;
-  depth: number;
+    url: URL;
+    depth: number;
 }
 
 export interface PageInfoResponse {
-  statusCode: number;
-  statusText: string;
-  contentType: string;
-  robotsHeader: string;
+    statusCode: number;
+    statusText: string;
+    contentType: string;
+    robotsHeader: string;
 }
 
 export interface ResultItem {
-  url: string;
-  statusCode: number;
-  statusText: string;
-  contentType: string;
-  isInternal: boolean;
-  depth: number;
-  redirectUrl?: string;
-  indexability?: boolean;
-  indexabilityStatus?: string;
-  resourceInfo?: PageResourceType[];
+    url: string;
+    statusCode: number;
+    statusText: string;
+    contentType: string;
+    isInternal: boolean;
+    depth: number;
+    redirectUrl?: string;
+    indexability?: boolean;
+    indexabilityStatus?: string;
+    resourceInfo?: PageResourceType[];
 }
 
 export interface PageResourceType {
-  type: string;
-  count: number;
-  broken: string[];
+    type: string;
+    count: number;
+    broken: string[];
 }
 
 export interface ExtractIndexability {
-  isIndexable: boolean;
-  indexabilityStatus: string;
+    isIndexable: boolean;
+    indexabilityStatus: string;
 }
--- src/types/robots-parser.d.ts
+++ src/types/robots-parser.d.ts
@@ -1,7 +1,7 @@
 export interface RobotsParser {
-  isAllowed: (pageUrl: string, userAgent: string) => boolean | undefined;
-  isDisallowed: (pageUrl: string, userAgent: string) => boolean | undefined;
-  getCrawlDelay: (userAgent: string) => number;
-  getSitemaps: () => string[];
-  getPreferedHost(): string;
+    isAllowed: (pageUrl: string, userAgent: string) => boolean | undefined;
+    isDisallowed: (pageUrl: string, userAgent: string) => boolean | undefined;
+    getCrawlDelay: (userAgent: string) => number;
+    getSitemaps: () => string[];
+    getPreferedHost(): string;
 }
