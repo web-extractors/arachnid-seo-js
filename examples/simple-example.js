ccd7a4749505603d6659e55ab5d29472751cbe41
Zeid Rashwani <zrashwani@gmail.com>
2022-05-08 16:16:07 +0300

    updating examples format

--- examples/simple-example.js
+++ examples/simple-example.js
@@ -1,27 +1,28 @@
-const Arachnid = require('@web-extractors/arachnid-seo').default;
+const Arachnid = require('@web-extractors/arachnid-seo').default
 
-async function crawl(domain, depth, concurrency) {
-    const startTime = Date.now();
-    const arachnidObj = new Arachnid(domain)
-        .setConcurrency(concurrency)
-        .setCrawlDepth(depth)
-        .shouldFollowSubdomains(true)
-        .setPuppeteerOptions({
-            args: [
-            '--disable-gpu',
-            '--disable-dev-shm-usage',
-            '--disable-setuid-sandbox',
-            '--no-first-run',
-            '--no-sandbox',
-            '--no-zygote',
-            '--single-process',
-        ]})
-    const results = await arachnidObj.traverse();
+async function crawl (domain, depth, concurrency) {
+  const startTime = Date.now()
+  const arachnidObj = new Arachnid(domain)
+    .setConcurrency(concurrency)
+    .setCrawlDepth(depth)
+    .shouldFollowSubdomains(true)
+    .setPuppeteerOptions({
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-sandbox',
+        '--no-zygote',
+        '--single-process'
+      ]
+    })
+  const results = await arachnidObj.traverse()
 
-    const endTime = Date.now();
-    console.log(JSON.stringify([...results], null, 4)); 
-    console.log("It took: " + (endTime - startTime) + " milliseconds");
-    console.log(`Found ${results.size} results with depth=${depth} concurrency=${concurrency}`);
+  const endTime = Date.now()
+  console.log(JSON.stringify([...results], null, 4))
+  console.log('It took: ' + (endTime - startTime) + ' milliseconds')
+  console.log(`Found ${results.size} results with depth=${depth} concurrency=${concurrency}`)
 }
 
-crawl("https://www.example.com/", 2, 5);
+crawl('https://www.example.com/', 2, 5)
